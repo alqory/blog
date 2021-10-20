2ddd9fac4888e0ba7d484d28694a9b4b4bbe8648
alqory <teamhendri18@gmail.com>
2021-10-20 13:54:43 +0700

    Membuat models dengan postgress sql

--- myblog/blog/models.py
+++ myblog/blog/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from ckeditor.fields import RichTextField
 
 # Create your models here.
+
+class blog(models.Model):
+    title           = models.CharField(max_length=123)
+    author          = models.CharField(max_length=50)
+    describ         = RichTextField(blank=True, null=True)
+    category        = models.CharField(max_length=50)
+    is_recomended   = models.BooleanField(default=False)
+    create          = models.DateTimeField(auto_now_add=True)
+    update          = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return "{} - {} ".format(self.title,self.author)
