2091731ecf0ce2918d062251a6c308d544fda8c1
alqory <teamhendri18@gmail.com>
2021-10-28 20:06:34 +0700

    Menambah fitur

--- myblog/myblog/settings.py
+++ myblog/myblog/settings.py
@@ -134,3 +134,8 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+import os
+MEDIA_URL = '/media/'
+# MEDIA_ROOT = [BASE_DIR,'media']
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
