5221a64c481761ffb607d102f7124f49fdd72c2c
alqory <teamhendri18@gmail.com>
2021-11-02 20:04:28 +0700

    Menambah fitur prakiraan cuaca

--- myblog/blog/views.py
+++ myblog/blog/views.py
@@ -2,18 +2,36 @@ from django.shortcuts import render, redirect
 from .models import *
 from .forms import *
 from django.core.paginator import Paginator
+import requests
+import math
 # Create your views here.
 
 def index(request):
+    # Cuaca API
+    city = 'Pontianak'
+    url = f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid=ebdd290488e624510dd716d774523aaa'
+    data = requests.get(url).json()
+    pypload = {
+        'kota':data['name'],
+        'ikon':data['weather'][0]['icon'],
+        'kecepatan_angin':data['wind']['speed'],
+        'temperatur': math.ceil(data['main']['temp'] - 273), #Celcius 
+        'tekanan_udara':data['main']['pressure'],
+        'kondisi':data['weather'][0]['main']
+        }
+    print(pypload)
+
+
     
-    blogs = blog.objects.all()
+    blogs = blog.objects.all().order_by('id')
     paginator = Paginator(blogs, 4)
     kategori = blog.objects.values('category').distinct()
 
-    page_number = request.GET.get('page',1)
+    page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
 
     context = {
+        'data':pypload,
         'blog':page_obj,
         'kat':kategori
         }
